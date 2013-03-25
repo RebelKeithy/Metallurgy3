509a606885e5c28a1db6bc5a98d1114b04c599d3
Vyrebird <vyre.public+git@gmail.com>
2013-03-25 05:20:57 -0400

    Further Armor Texture Fix
    Fix case mismatch between textures and calculated names
    Fix metal set names containing spaces

--- common/rebelkeithy/mods/metallurgy/core/metalsets/ItemMetallurgyArmor.java
+++ common/rebelkeithy/mods/metallurgy/core/metalsets/ItemMetallurgyArmor.java
@@ -9,19 +9,26 @@ import net.minecraftforge.common.IArmorTextureProvider;
 public class ItemMetallurgyArmor extends ItemArmor implements IArmorTextureProvider{
 
 	public String textureFile;
-	
+
 	public ItemMetallurgyArmor(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4) {
 		super(par1, par2EnumArmorMaterial, par3, par4);
 	}
-	
+
 	public Item setTextureFile(String texture)
 	{
-		textureFile = texture;
+		int loc = texture.indexOf(' ');
+		while(loc!=-1)
+		{
+			texture = new StringBuilder(texture).deleteCharAt(loc).toString();
+			loc = texture.indexOf(' ');
+		}
+
+		textureFile = texture.toLowerCase();
 		return this;
 	}
 
 	@Override
-	public String getArmorTextureFile(ItemStack itemstack) 
+	public String getArmorTextureFile(ItemStack itemstack)
 	{
 		return "/armor/" + textureFile + ".png";
 	}
