58ef84a9ad8e91ba6955f88cd15a06c83fe86877
Smeagolworms4 <smeagolworms4@gmail.com>
2014-01-08 21:50:04 +0100

    meta depacant 16

--- common/rebelkeithy/mods/metallurgy/compatibility/TileEntityMintCompatibility.java
+++ common/rebelkeithy/mods/metallurgy/compatibility/TileEntityMintCompatibility.java
@@ -1,7 +1,20 @@
 package rebelkeithy.mods.metallurgy.compatibility;
 
+import net.minecraft.nbt.NBTTagCompound;
 import rebelkeithy.mods.metallurgy.machines.mint.TileEntityMint;
 
 public class TileEntityMintCompatibility extends TileEntityMint {
-
+	
+	@Override
+    public void readFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        try {
+        	super.readFromNBT(par1NBTTagCompound);
+        } catch (Exception e) {
+        	direction = par1NBTTagCompound.getInteger ("Direction");
+	        ingotId = par1NBTTagCompound.getInteger("Ingot");
+	        amount = par1NBTTagCompound.getInteger("Amount");
+        }
+    }
+	
 }
--- common/rebelkeithy/mods/metallurgy/compatibility/TileEntityMintStorageCompatibility.java
+++ common/rebelkeithy/mods/metallurgy/compatibility/TileEntityMintStorageCompatibility.java
@@ -7,4 +7,33 @@ import rebelkeithy.mods.metallurgy.machines.mint.TileEntityMintStorage;
 
 public class TileEntityMintStorageCompatibility extends TileEntityMintStorage {
 	
+	/**
+     * Reads a tile entity from NBT.
+     */
+    @Override
+    public void readFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+    	try {
+    		super.readFromNBT(par1NBTTagCompound);
+        } catch (Exception e) {
+        	direction = par1NBTTagCompound.getShort ("Direction");
+        	
+        	final NBTTagList var2 = par1NBTTagCompound.getTagList("Items");
+            chestContents = new ItemStack[getSizeInventory()];
+
+            for (int var3 = 0; var3 < var2.tagCount(); ++var3)
+            {
+                final NBTTagCompound var4 = (NBTTagCompound) var2.tagAt(var3);
+                final int var5 = var4.getByte("Slot") & 255;
+
+                if (var5 >= 0 && var5 < chestContents.length)
+                {
+                    chestContents[var5] = ItemStack.loadItemStackFromNBT(var4);
+                }
+            }
+        }
+        
+        
+
+    }
 }
